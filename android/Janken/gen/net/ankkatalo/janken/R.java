4ffb2d75b4efebe701a866b3469acfd25c1581d2
Jussi Niskanen <jussini@gmail.com>
2012-08-09 15:25:07 +0300

    started graphics overhaul

--- android/Janken/gen/net/ankkatalo/janken/R.java
+++ android/Janken/gen/net/ankkatalo/janken/R.java
@@ -11,28 +11,32 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int paper=0x7f020001;
-        public static final int paper_lose=0x7f020002;
-        public static final int paper_win=0x7f020003;
-        public static final int rock=0x7f020004;
-        public static final int rock_lose=0x7f020005;
-        public static final int rock_win=0x7f020006;
-        public static final int scissors=0x7f020007;
-        public static final int scissors_lose=0x7f020008;
-        public static final int scissors_win=0x7f020009;
-        public static final int unknown=0x7f02000a;
+        public static final int background_lanscape=0x7f020000;
+        public static final int background_portrait=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int paper=0x7f020003;
+        public static final int paper_lose=0x7f020004;
+        public static final int paper_win=0x7f020005;
+        public static final int rock=0x7f020006;
+        public static final int rock_lose=0x7f020007;
+        public static final int rock_win=0x7f020008;
+        public static final int scissors=0x7f020009;
+        public static final int scissors_lose=0x7f02000a;
+        public static final int scissors_win=0x7f02000b;
+        public static final int unknown=0x7f02000c;
+        public static final int vstext=0x7f02000d;
     }
     public static final class id {
-        public static final int clearHistoryItem=0x7f060007;
-        public static final int clearStatsItem=0x7f060008;
+        public static final int clearHistoryItem=0x7f060008;
+        public static final int clearStatsItem=0x7f060009;
         public static final int cpuImageView=0x7f060000;
         public static final int paperButton=0x7f060004;
         public static final int playerImageView=0x7f060002;
         public static final int rockButton=0x7f060003;
         public static final int scissorsButton=0x7f060005;
         public static final int textView=0x7f060006;
-        public static final int textView1=0x7f060001;
+        public static final int textView1=0x7f060007;
+        public static final int vstextImage=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
