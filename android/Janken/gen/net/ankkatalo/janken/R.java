43efafa481acd2ef4a5709c1757e6178773b8a8c
Jussi Niskanen <jussini@gmail.com>
2012-08-15 15:49:39 +0300

    made imagebutton graphics have 2 states, yet another graphics for buttons. this time the theme will stay. sure.

--- android/Janken/gen/net/ankkatalo/janken/R.java
+++ android/Janken/gen/net/ankkatalo/janken/R.java
@@ -16,18 +16,24 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
         public static final int paper=0x7f020003;
         public static final int paper_lose=0x7f020004;
-        public static final int paper_tie=0x7f020005;
-        public static final int paper_win=0x7f020006;
-        public static final int rock=0x7f020007;
-        public static final int rock_lose=0x7f020008;
-        public static final int rock_tie=0x7f020009;
-        public static final int rock_win=0x7f02000a;
-        public static final int scissors=0x7f02000b;
-        public static final int scissors_lose=0x7f02000c;
-        public static final int scissors_tie=0x7f02000d;
-        public static final int scissors_win=0x7f02000e;
-        public static final int unknown=0x7f02000f;
-        public static final int vstext=0x7f020010;
+        public static final int paper_pressed=0x7f020005;
+        public static final int paper_selector=0x7f020006;
+        public static final int paper_tie=0x7f020007;
+        public static final int paper_win=0x7f020008;
+        public static final int rock=0x7f020009;
+        public static final int rock_lose=0x7f02000a;
+        public static final int rock_pressed=0x7f02000b;
+        public static final int rock_selector=0x7f02000c;
+        public static final int rock_tie=0x7f02000d;
+        public static final int rock_win=0x7f02000e;
+        public static final int scissors=0x7f02000f;
+        public static final int scissors_lose=0x7f020010;
+        public static final int scissors_pressed=0x7f020011;
+        public static final int scissors_selector=0x7f020012;
+        public static final int scissors_tie=0x7f020013;
+        public static final int scissors_win=0x7f020014;
+        public static final int unknown=0x7f020015;
+        public static final int vstext=0x7f020016;
     }
     public static final class id {
         public static final int clearHistoryItem=0x7f060007;
